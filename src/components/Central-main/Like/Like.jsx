812e55563b21032357cdd6d49fc701def4dc2a9f
JhonMeme <emerxxithoz@gmail.com>
2023-09-21 03:12:37 -0500

    more

--- src/components/Central-main/Like/Like.jsx
+++ src/components/Central-main/Like/Like.jsx
@@ -7,18 +7,17 @@ const Like = () =>{
             <div className="footer">
                 <div className="footer-icons">
                     <img className="corazon" id="corazon" src="src\assets\images\assets-inicio\iconos\corazon.png" alt="" />
-                    <span className="burbuja" id="burbuja"></span>
-                    <span className="enviar" id="enviar"></span>
-
+                    <img className="burbuja" id="burbuja" src="src/assets/images/assets-inicio/iconos/burbuja.png" alt="" />
+                    <img className="enviar" id="enviar" src="src\assets\images\assets-inicio\iconos\enviar.png" alt="" />
                     <div className="guardar-icon-container">
-                        <span className="guardar" id="guardar"></span>
+                        <img className="guardar" id="guardar" src="src\assets\images\assets-inicio\iconos\guardar.png" alt="" />
                     </div>
 
                     <div className="caption-container">
                         <h4> <span>234</span> Me gusta </h4>
 
                         <div className="caption">
-                            <h4>Alejandro Lopez <span>Gracias por apoyarnos</span> </h4>
+                            <h4>Alejandro Lopez <span>Gracias por apoyar</span> </h4>
                         </div>
                     </div>
                 </div>
